20892c9521cacdd568515b583a4c3fd474e53bb6
realan <realan@users.noreply.github.com>
2020-12-31 14:29:50 +0500

    stock change table to DataGrid

--- src/views/Stock.js
+++ src/views/Stock.js
@@ -3,13 +3,13 @@ import { gql } from "apollo-boost";
 import { useSubscription } from "@apollo/react-hooks";
 import ReactTableExp from "../components/ReactTableExp/ReactTableExp";
 import DialogOrders from "../components/DialogOrders/DialogOrders.js";
-
+import { DataGrid } from '@material-ui/data-grid';
 // import { XGrid } from '@material-ui/x-grid';
 
 const SUBSCRIPTION_STOCK = gql`
   subscription {
     mr_pivot {
-      item_id
+      id: item_id
       item_name
       stock_now
       order_this_week
@@ -23,36 +23,36 @@ const SUBSCRIPTION_STOCK = gql`
 
 const Stock = () => {
 
-  const columns = React.useMemo(
-    () => [
-      {Header: "id", accessor: "item_id", type: "integer", show: false, required: true},
-      {Header: "Название", accessor: "item_name", type: "text", show: true, required: true},
-      {Header: "На складе", accessor: "stock_now", type: "integer", show: true, required: true},
-      {Header: "Эта неделя", columns: [  
-        {Header: "Заказано", accessor: "order_this_week", type: "integer", show: true, required: true},
-        {Header: "Набрано", accessor: "collected_this_week", type: "integer", show: true, required: true},
-      ]}, 
-      {Header: "Следующая неделя", columns: [  
-        {Header: "Заказано", accessor: "order_next_week", type: "integer", show: true, required: true},
-        {Header: "Набрано", accessor: "collected_next_week", type: "integer", show: true, required: true},
-      ]},
-      {Header: "Заказ позже", accessor: "order_next", type: "integer", show: true, required: true},
-    ],
-    []
-  );
+  // const columns = React.useMemo(
+  //   () => [
+  //     {Header: "id", accessor: "item_id", type: "integer", show: false, required: true},
+  //     {Header: "Название", accessor: "item_name", type: "text", show: true, required: true},
+  //     {Header: "На складе", accessor: "stock_now", type: "integer", show: true, required: true},
+  //     {Header: "Эта неделя", columns: [  
+  //       {Header: "Заказано", accessor: "order_this_week", type: "integer", show: true, required: true},
+  //       {Header: "Набрано", accessor: "collected_this_week", type: "integer", show: true, required: true},
+  //     ]}, 
+  //     {Header: "Следующая неделя", columns: [  
+  //       {Header: "Заказано", accessor: "order_next_week", type: "integer", show: true, required: true},
+  //       {Header: "Набрано", accessor: "collected_next_week", type: "integer", show: true, required: true},
+  //     ]},
+  //     {Header: "Заказ позже", accessor: "order_next", type: "integer", show: true, required: true},
+  //   ],
+  //   []
+  // );
 
-  // const columnsX = React.useMemo( () =>
-  // [
-  //   { field: 'item_id', headerName: 'id', width: 30 },
-  //   { field: 'item_name', headerName: 'Название', width: 200 },
-  //   { field: 'stock_now', headerName: 'Column 2', width: 50 },
-  //   { field: 'order_this_week', headerName: 'Column 2', width: 60 },
-  //   { field: 'collected_this_week', headerName: 'Column 2', width: 30 },
-  //   { field: 'order_next_week', headerName: 'Column 2', width: 30 },
-  //   { field: 'collected_next_week', headerName: 'Column 2', width: 30 },
-  //   { field: 'order_next', headerName: 'Column 2', width: 30 },
-  // ]
-  // , []);
+  const columns = React.useMemo( () =>
+  [
+    { field: 'id', headerName: 'id', width: 30 },
+    { field: 'item_name', headerName: 'Название', width: 200 },
+    { field: 'stock_now', headerName: 'Склад', type: "number", width: 110 },
+    { field: 'order_this_week', headerName: 'Заказ 1', type: "number", width: 110 },
+    { field: 'collected_this_week', headerName: 'Набрано 1', type: "number", width: 110 },
+    { field: 'order_next_week', headerName: 'Заказ 2', type: "number", width: 110 },
+    { field: 'collected_next_week', headerName: 'Набрано 2', type: "number", width: 110 },
+    { field: 'order_next', headerName: 'Заказ далее', type: "number", width: 110 },
+  ]
+  , []);
 
     //For modal dialog window
     const [open, setOpen] = React.useState(false);
@@ -72,26 +72,20 @@ const Stock = () => {
   if (loading) return "Loading....";
   if (error) return `Error! ${error.message}`;
 
-  const dataTable = data.mr_pivot;
- 
-  // const tableData=data.mr_pivot;
+  const tableData=data.mr_pivot;
+
   // const rows = tableData.map( (obj) => {
   //   obj['id'] = obj['item_id'];
   //   delete obj['item_id'];
   //   return obj;
   // });
-  // console.log(rows)
+  console.log(tableData)
 
   return (
     <div>
-      <ReactTableExp
-        columns={columns}
-        data={dataTable}
-        onRowClick={onRowClick}
-      />
-
-      {/* <XGrid rows={rows} columns={columnsX} /> */}
-      
+      <div style={{ height: 700, width: '100%' }}>
+        <DataGrid rows={tableData} columns={columns} />
+      </div>
       <DialogOrders
         open={open}
         handleClose ={handleClose}
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,15 +1,105 @@
-import React from "react";
+import React  from "react";
+import { gql } from "apollo-boost";
+import { useSubscription } from "@apollo/react-hooks";
+import ReactTableExp from "../components/ReactTableExp/ReactTableExp";
+import DialogOrders from "../components/DialogOrders/DialogOrders.js";
 
-// import { Row, Col } from "reactstrap";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import contentData from "../utils/contentData";
+// import { XGrid } from '@material-ui/x-grid';
 
-const Orders = () => {
+const SUBSCRIPTION_STOCK = gql`
+  subscription {
+    mr_pivot {
+      item_id
+      item_name
+      stock_now
+      order_this_week
+      collected_this_week
+      order_next_week
+      collected_next_week
+      order_next
+    }
+  }
+`;
+
+const Todo = () => {
+
+  const columns = React.useMemo(
+    () => [
+      {Header: "id", accessor: "item_id", type: "integer", show: false, required: true},
+      {Header: "Название", accessor: "item_name", type: "text", show: true, required: true},
+      {Header: "На складе", accessor: "stock_now", type: "integer", show: true, required: true},
+      {Header: "Эта неделя", columns: [  
+        {Header: "Заказано", accessor: "order_this_week", type: "integer", show: true, required: true},
+        {Header: "Набрано", accessor: "collected_this_week", type: "integer", show: true, required: true},
+      ]}, 
+      {Header: "Следующая неделя", columns: [  
+        {Header: "Заказано", accessor: "order_next_week", type: "integer", show: true, required: true},
+        {Header: "Набрано", accessor: "collected_next_week", type: "integer", show: true, required: true},
+      ]},
+      {Header: "Заказ позже", accessor: "order_next", type: "integer", show: true, required: true},
+    ],
+    []
+  );
+
+  // const columnsX = React.useMemo( () =>
+  // [
+  //   { field: 'item_id', headerName: 'id', width: 30 },
+  //   { field: 'item_name', headerName: 'Название', width: 200 },
+  //   { field: 'stock_now', headerName: 'Column 2', width: 50 },
+  //   { field: 'order_this_week', headerName: 'Column 2', width: 60 },
+  //   { field: 'collected_this_week', headerName: 'Column 2', width: 30 },
+  //   { field: 'order_next_week', headerName: 'Column 2', width: 30 },
+  //   { field: 'collected_next_week', headerName: 'Column 2', width: 30 },
+  //   { field: 'order_next', headerName: 'Column 2', width: 30 },
+  // ]
+  // , []);
+
+    //For modal dialog window
+    const [open, setOpen] = React.useState(false);
+    const [itemId, setItemId] = React.useState(undefined);
+    const [stockQty, setStockQty] = React.useState(undefined);
+
+    const onRowClick = (row) => {
+      setItemId(row.values.item_id);
+      setStockQty(row.values.stock_now);
+      setOpen(true);
+    }
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+  const { loading, error, data } = useSubscription(SUBSCRIPTION_STOCK);
+  if (loading) return "Loading....";
+  if (error) return `Error! ${error.message}`;
+
+  const dataTable = data.mr_pivot;
+ 
+  // const tableData=data.mr_pivot;
+  // const rows = tableData.map( (obj) => {
+  //   obj['id'] = obj['item_id'];
+  //   delete obj['item_id'];
+  //   return obj;
+  // });
+  // console.log(rows)
+
   return (
     <div>
-      ToDo
+      <ReactTableExp
+        columns={columns}
+        data={dataTable}
+        onRowClick={onRowClick}
+      />
+
+      {/* <XGrid rows={rows} columns={columnsX} /> */}
+      
+      <DialogOrders
+        open={open}
+        handleClose ={handleClose}
+        item_id={itemId}
+        stock_now={stockQty}
+      />
     </div>
   )
 }
 
-export default Orders;
+export default Todo;
