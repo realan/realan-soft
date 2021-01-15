8e86683b8af2b39d26d063165798f2a3e1964f25
realan <realan@bk.ru>
2021-01-15 19:13:43 +0500

    inputgroup state

--- src/components/InputGroup/InputGroup.js
+++ src/components/InputGroup/InputGroup.js
@@ -9,65 +9,116 @@ import OutlinedInput from "@material-ui/core/OutlinedInput";
 import Fab from "@material-ui/core/Fab";
 import { Box } from "@material-ui/core";
 
-const InputGroup = (props) => {
+const InputGroup = ({id, minimValue, maxValue, type, onChange}) => {
   console.log("render InputGroup")
   // props id, type (stock or prod), maxValue, minValue onChange -
   // const [count, setCount] = useState(0);
+  const minValue = minimValue || 0;
+
   const [state, setState] = useState({
     count: 0,
-    disabledDecrease: 0 <= props.minValue ? true : false,
-    disabledIncrease: 0 >= props.maxValue ? true : false,
-    disabledMAxValue: 0 >= props.maxValue ? true : false,
+    disabledDecrease: 0 <= minValue ? true : false,
+    disabledIncrease: 0 >= maxValue ? true : false,
+    disabledMAxValue: 0 >= maxValue ? true : false,
   });
 
-  const minValue = props.minValue || 0;
-  const color = props.type === "stock" ? "primary" : "default";
+  const color = type === "stock" ? "primary" : "default";
 
-  const increase = () => {
-    if (state.count + 1 > props.maxValue) {
+
+  // const increase = () => {
+  //   if (state.count + 1 > maxValue) {
+  //   } else {
+  //     onChange(id, state.count + 1, type);
+  //     // setCount(count + 1);
+  //     let c = state.count + 1;
+  //     setState({
+  //       count: c,
+  //       disabledDecrease: c <= minValue ? true : false,
+  //       disabledIncrease: c >= maxValue ? true : false,
+  //       disabledMAxValue: c >= maxValue ? true : false,
+  //     });
+  //   }
+  // };
+
+  // const decrease = () => {
+  //   if (state.count - 1 >= minValue) {
+  //     onChange(id, state.count - 1, type);
+  //     // setCount(count - 1);
+  //     let c = state.count - 1;
+  //     setState({
+  //       count: c,
+  //       disabledDecrease: c <= minValue ? true : false,
+  //       disabledIncrease: c >= maxValue ? true : false,
+  //       disabledMAxValue: c >= maxValue ? true : false,
+  //     });
+  //   }
+  // };
+
+  
+  // const setMaxValue = () => {
+  //   // setCount(props.maxValue);
+  //   setState({...state, count: maxValue})
+  //   onChange(id, maxValue, type);
+  // };
+
+  // //   console.log(props)
+
+  const onClick = (event) => {
+    const buttonType = "increase"
+    const elemId = event.target.id;
+    console.log(event)
+    let c = 0;
+    let flag = true;
+    if (buttonType === 'increase') {
+      c = state.count + 1;
+      (c > maxValue) ? flag = false : flag = true;
+    } else if (buttonType === 'decrease') {
+      c = state.count - 1;
+      // console.log(c);
+      (c < minValue) ? flag = false : flag = true;
     } else {
-      props.onChange(props.id, state.count + 1, props.type);
-      // setCount(count + 1);
-      setState({...state, count: state.count + 1})
+      c = maxValue;
     }
-  };
-
-  const decrease = () => {
-    if (state.count - 1 >= minValue) {
-      props.onChange(props.id, state.count - 1, props.type);
-      // setCount(count - 1);
-      setState({...state, count: state.count - 1})
+    console.log(buttonType, flag, c)
+    if (flag) {
+      setState({
+        count: c,
+        disabledDecrease: c <= minValue ? true : false,
+        disabledIncrease: c >= maxValue ? true : false,
+        disabledMaxValue: c >= maxValue ? true : false,
+      });
+      onChange(id, c, type);
     }
-  };
+  }
 
   const onChangeInput = (e) => {
     let val = e.target.value;
-    if (val >= minValue && val <= props.maxValue) {
+    if (val >= minValue && val <= maxValue) {
       // setCount(val);
-      setState({...state, count: val})
-      props.onChange(props.id, val, props.type);
+      let c = val;
+      setState({
+        count: c,
+        disabledDecrease: c <= minValue ? true : false,
+        disabledIncrease: c >= maxValue ? true : false,
+        disabledMAxValue: c >= maxValue ? true : false,
+      });
+      onChange(id, c, type);
     }
   };
 
-  const setMaxValue = () => {
-    // setCount(props.maxValue);
-    setState({...state, count: props.maxValue})
-    props.onChange(props.id, props.maxValue, props.type);
-  };
-
-  //   console.log(props)
 
 
 
   return (
     <div>
       <Fab
+        id="decrease"
         color={color}
         aria-label="add"
         size="medium"
         component="span"
-        onClick={decrease}
-        disabled={state.count <= props.minValue ? true : false}
+        onClick={()=>onClick("decrease")}
+        disabled={state.count <= minValue ? true : false}
       >
         <RemoveIcon />
       </Fab>
@@ -82,22 +133,24 @@ const InputGroup = (props) => {
       </Box>
 
       <Fab
+        id="increase"
         color={color}
         aria-label="add"
         size="medium"
         component="span"
-        onClick={increase}
-        disabled={state.count >= props.maxValue ? true : false}
+        onClick={onClick}
+        disabled={state.count >= maxValue ? true : false}
       >
         <AddIcon />
       </Fab>
       <Fab
+        id="maxValue"
         color={color}
         aria-label="add"
         size="medium"
         component="span"
-        onClick={setMaxValue}
-        disabled={state.count >= props.maxValue ? true : false}
+        onClick={()=>onClick("maxValue")}
+        disabled={state.count >= maxValue ? true : false}
       >
         <DoneAllIcon />
       </Fab>
